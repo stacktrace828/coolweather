0cedc6dd11cefc1ef496326029bedfe1636253c0
stacktrace828 <stacktrace828@gmail.com>
2016-02-10 15:33:28 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/stacktrace/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.stacktrace.coolweather.model;
+
+/**
+ * Created by stacktrace on 2016/2/10.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
